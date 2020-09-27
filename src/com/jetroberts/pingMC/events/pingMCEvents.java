c8ee05a01817091bd2edc96fad770be3f214f5c9
JoshRoberts <j-et@live.co.uk>
2020-09-27 22:24:29 +0100

    Currently adding function to allow random rotation of the armor stand.

--- src/com/jetroberts/pingMC/events/pingMCEvents.java
+++ src/com/jetroberts/pingMC/events/pingMCEvents.java
@@ -5,15 +5,15 @@ import com.jetroberts.pingMC.pingMC;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.EulerAngle;
 
 import java.util.Objects;
 
@@ -45,11 +45,21 @@ public class pingMCEvents implements Listener {
             Location newTargetLocation = targetBlock.getLocation();
             newTargetLocation.setY(newTargetLocation.getY() + 1);
 
-            Block currentBlock = newTargetLocation.getBlock();
-            if(checkThatBlockIsAir(currentBlock)) {
+            if(checkThatBlockIsAir(newTargetLocation)) {
                 ArmorStand stand = (ArmorStand) player.getWorld().spawnEntity(newTargetLocation, EntityType.ARMOR_STAND);
-                stand.setMarker(true);
+                Objects.requireNonNull(stand.getEquipment()).setItemInMainHand(new ItemStack(Material.REDSTONE));
+
+                stand.setRightArmPose(new EulerAngle(Math.toRadians(50), Math.toRadians(0), Math.toRadians(0)));
+                stand.setHeadPose(new EulerAngle(Math.toRadians(17), Math.toRadians(9), Math.toRadians(0)));
+
                 stand.setVisible(false);
+                stand.setInvulnerable(true);
+                stand.setMarker(true);
+                stand.setGlowing(false);
+                stand.setBasePlate(false);
+                stand.setSilent(true);
+                stand.setGravity(false);
+
                 delayedDelete(stand);
             }
         }
@@ -63,10 +73,10 @@ public class pingMCEvents implements Listener {
             }
         };
 
-        task.runTaskLater(this.plugin, 20 * 4);
+        task.runTaskLater(this.plugin, 20 * 6);
     }
 
-    private boolean checkThatBlockIsAir(Block currentBlock) {
-        return currentBlock.getType() == Material.AIR;
+    private boolean checkThatBlockIsAir(Location newTargetLocation) {
+        return newTargetLocation.getBlock().getType() == Material.AIR;
     }
 }
